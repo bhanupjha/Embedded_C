4b1d41fa64689edd7630d65e5381f9628c605994
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-05 20:45:18 +0530

    ASCII value for character

---
+++ ASCII.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("Enter the character: ");
+	scanf("%c", &a);
+	printf("Your Entered character is: %c\n", a);
+	printf("Entered character %c ASCII Value is: %d\n", a, a); 
+
+}
