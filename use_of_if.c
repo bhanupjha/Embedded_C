8dc2f860e0ac1e77f56277f6c270073d664722f0
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-06 13:16:38 +0530

    if statement program

---
+++ use_of_if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int x;
+	printf("Enter the Number: ");
+	scanf("%d", &x);
+	if (x>0)
+	{	
+		printf("it is positive number");
+	}
+	else
+	{
+		printf("it is negative number");
+	}
+}
