86aa2f5bcdef7394bf0f8e6cb91891870ed1c56d
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-15 00:51:11 +0530

    comments in c and escape sequence used, seperator

---
+++ comments.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	// for single line comment use // 
+	/* For multi 
+	           line
+	     use 
+	                       this */
+	printf("hello\n"); // \n is an escape sequence that is used for new line
+	printf("\tBhanu\n"); // \t is an escape sequence used for tab space
+	printf("hii, This is seperator or delimeter"); /* ,  (space) is delimeter or seperator used to provide space or  
+							  diffrentiate between two words */
+
+}
