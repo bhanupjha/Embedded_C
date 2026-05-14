43446265d1e7a74e0662c2763b3ed3a6bd82081a
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-15 00:46:08 +0530

    performed increment and decrement operation

---
+++ operators/unary_operator/pre_decrement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int num, y;
+	printf("Enter the value of num is: ");
+	scanf("%d", &num);
+	y = --num;
+	printf("The updated value of variable after pre-increment is:\nnum value is: %d\ny value is:%d\n", num, y);
+}
