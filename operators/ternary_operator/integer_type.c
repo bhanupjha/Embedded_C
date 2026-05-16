abb1603c432266b557842dcb25b51c029f5ee02c
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-16 12:25:18 +0530

    checking i/p value +ve or -ve

---
+++ operators/ternary_operator/integer_type.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int value;
+	printf("Enter the integer: ");
+	scanf("%d", &value);
+        value>=0?printf("The integer value is positive\n"):printf("The integer value is negative\n");
+
+}
