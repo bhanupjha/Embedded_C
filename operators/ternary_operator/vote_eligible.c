2352bd53051319adf3794f2fabb05b78b01e6e51
Bhanu Prakash <mailtobhanup@gmail.com>
2026-05-16 12:26:20 +0530

    checking eligibility for voting

---
+++ operators/ternary_operator/vote_eligible.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int age;
+	printf("Enter the your age: ");
+	scanf("%d", &age);
+	age>=18?printf("You are elgible for voting\n"):printf("You are not eligible for voting\n");
+
+
+}
