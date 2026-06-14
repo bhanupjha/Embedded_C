fe413e7d558c30e5592798a839397789655b8a50
Bhanu Prakash <mailtobhanup@gmail.com>
2026-06-14 17:58:15 +0530

    working on loops

---
+++ assignment_loops/2cond.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+ main() { int p;	 
+	for( p=1; p<=10,--p ; p =p+2 )
+	printf( " Hello " );
+printf( " hi " );
+ }
---
+++ assignment_loops/for_simp.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+main()
+{ int i;
+for (i=5; ++i; i -=3)
+printf("%d ",i);
+}
