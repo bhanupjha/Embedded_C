8207a94cbd7225f28e399e399d2c6ce10ff55417
Bhanu Prakash <mailtobhanup@gmail.com>
2026-09-03 23:06:20 +0530

    Creating Database using 2D array

---
+++ array/2D_array/nameDB.c
@@ -0,0 +1,109 @@
+// Creating a Name database
+
+#define ROW 5
+#define COL 20
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdio_ext.h>
+#include<string.h>
+
+void Insert(char (*)[COL]);
+void Print(char (*)[COL]);
+void Delete(char (*)[COL]);
+void Sort(char (*)[COL]);
+int cnt;
+int main()
+{
+	char DB[ROW][COL];
+	char choice;
+	while(1)
+	{
+		printf("What u want to perform?\n");
+		printf("Press: i. Insert, p. Print, d. Delete, s. Sort, e. exit\n");
+		printf("Enter your choice:\n");
+		__fpurge(stdin);
+		scanf("%c", &choice);
+		switch(choice)
+		{
+			case 'i': Insert(DB);
+				  break;
+			case 'p': Print(DB);
+				  break;
+			case 'd': Delete(DB);
+				  break;
+			case 's': Sort(DB);
+				  break;
+			case 'e': exit(0); 
+		}
+	}
+}
+
+void Insert(char (*p)[COL])
+{
+	if(cnt==ROW)
+	{
+		printf("DB is FULL\n");
+		return;
+	}
+	else
+	{
+		printf("Enter the name to be inserted:\n");
+		scanf("%s", p[cnt]);
+		cnt++;
+	}
+}
+
+void Print(char (*p)[COL])
+{
+        if(cnt==0)
+        {
+                printf("DB is Empty\n");
+                return;
+        }
+        else
+        {
+                for(int i=0; i<cnt; i++)
+		{
+			printf("%s\n", p[i]);
+		}
+        }
+}
+
+// Based on given string
+void Delete(char (*p)[COL])
+{
+	char str[COL];
+	printf("Enter the string to be deleted:\n");
+	__fpurge(stdin);
+	scanf("%[^\n]s", str);
+	for(int i=0; i<cnt; i++)
+	{
+		if (strcmp(str, p[i])==0)
+		{
+			memmove((p+i), (p+i+1), (cnt-i-1)*COL);
+			cnt--;
+			return;
+		}
+	}
+	printf("%s is not found", str);
+}
+
+void Sort(char (*p)[COL])
+{
+	char temp[COL];
+	for(int i=1; i<cnt; i++)
+	{
+		for(int j=0; j<cnt-i; j++)
+		{
+			int t;
+			if(t=(strcmp(p[j], p[j+1])))
+			{
+				printf("%d\n", t);
+			/*	strcpy(temp, p[j]);
+				strcpy(p[j], p[j+1]);
+				strcpy(p[j], temp);*/
+			}
+		}
+	}
+}
